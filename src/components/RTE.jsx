931ef0eda1384259f4668422b08ff7c86e456a50
sourav48765 <bisht4876@gmail.com>
2024-07-19 10:52:51 +0530

    trial again

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({name, control, label, defaultValue="" }) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey={`${conf.tinyMcApiId}`}
+        apiKey={conf.tinyMcApiId}
         initialValue={defaultValue}
         init={{
             height:500,
